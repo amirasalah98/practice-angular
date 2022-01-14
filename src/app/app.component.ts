44d9619716800e30b92747d635e6524fe546028b
amirasalah98 <amira.salahhh98@gmail.com>
2022-01-14 00:28:31 -0800

    ang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+// import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'blog';
+  faCoffee = faCoffee;
+
+   login={
+    av: true
+  }
+  
+  change(){
+    this.login.av=!this.login.av 
+    console.log(this.login.av)
+  }
+
+ 
 }
